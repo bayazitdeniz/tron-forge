5532e45b427e70eb459240c90c1f34e23d9efa46
bayazitdeniz <bayazitdeniz@gmail.com>
2021-04-12 21:03:00 -0400

    added losing stutter but still haven't solved switching players

--- tron.rkt
+++ tron.rkt
@@ -17,9 +17,11 @@ abstract sig Player {
 one sig P1, P2 extends Player {}
 
 one sig Board {
-    var walls: set Idx->Idx
+    var walls: set Idx->Idx,
+    var currPlayer: one Player
 }
 
+// TODO: change fixed player start?
 pred initFirstAndLast {
     no walls
     --
@@ -28,54 +30,96 @@ pred initFirstAndLast {
     --
     P2.row = LastIdx
     P2.col = LastIdx
-}   
+}
 
-pred moveTo[player: Player, nextRow: Idx, nextCol: Idx] {
-    // something above
+fun otherPlayer[p : Player] : Player {
+    Player - p
+}
+
+pred switchPlayers {
+    Board.(currPlayer') = otherPlayer[Board.currPlayer]
+}
+
+pred moveTo[p: Player, nextRow: Idx, nextCol: Idx] {
+    // pre-conditions
+    // some next loc - next relation is lone!
     some nextRow
     some nextCol
-    // no wall above
+    // no wall in the next location
     not (nextRow->nextCol in Board.walls)
+    // no player in the next location
+    let otherP = otherPlayer[p] | {
+        nextRow != otherP.row
+        nextCol != otherP.col
+    }
 
-    // postconditions
+    // post-conditions
     // curr loc will be in walls
-    Board.(walls') = Board.walls + (player.row->player.col)
+    Board.(walls') = Board.walls + (p.row->p.col)
 
-    row' = (row - player->Idx) + player->nextRow
-    col' = (row - player->Idx) + player->nextCol
+    row' = (row - p->Idx) + p->nextRow
+    col' = (col - p->Idx) + p->nextCol
 }
 
-pred moveUp[player: Player] {
-    moveTo[player, next.(player.row), player.col]
+pred moveUp[p: Player] {
+    moveTo[p, next.(p.row), p.col]
 }
 
-pred moveDown[player : Player] {
-    moveTo[player, (player.row).next, player.col]
+pred moveDown[p : Player] {
+    moveTo[p, (p.row).next, p.col]
 }
 
-pred moveLeft[player: Player] {
-    moveTo[player, player.row, next.(player.col)]
+pred moveLeft[p: Player] {
+    moveTo[p, p.row, next.(p.col)]
 }
 
-pred moveRight[player : Player] {
-    moveTo[player, player.row, (player.col).next]
+pred moveRight[p : Player] {
+    moveTo[p, p.row, (p.col).next]
 }
 
-pred winnerStutter {
-    // todo!
+// pred testTraces {
+//     moveUp[P2]
+//     after moveLeft[P2]
+//     after after moveDown[P2]
+//     after after after moveLeft[P2]
+//     after after after after moveUp[P2]
+//     after after after after after moveUp[P2]
+//     after after after after after after {
+//         moveRight[P2]
+//         until no P2.col.next
+// }
+
+pred loserStutter[p : Player] {
+    let r = p.row | 
+        let c = p.col | 
+            let adjacents = (r.next)->c + (next.r)->c + r->(c.next) + r->(next.c) - none->Idx - Idx->none |
+                let oP = otherPlayer[p]|
+                    no (adjacents - Board.walls - (oP.row)->(oP.col))
+    walls' = walls
+    row' = row 
+    col' = col
+    currPlayer' = currPlayer
 }
 
 pred traces {
-    moveUp[P2]
-    after moveLeft[P2]
-    after after moveDown[P2]
-    after after after moveLeft[P2]
-    after after after after moveUp[P2]
-    after after after after after moveUp[P2]
-    after after after after after after {
-        moveRight[P2]
-        until no P2.col.next
-    }
+    always (
+        moveUp[Board.currPlayer] or moveDown[Board.currPlayer] or moveLeft[Board.currPlayer] or moveRight[Board.currPlayer]
+        or loserStutter[Board.currPlayer]
+    )
+}
+
+inst threeByThreeBoard {
+    Idx = I1 + I2 + I3
+    FirstIdx = I1
+    LastIdx = I3
+    next = I1->I2 + I2->I3
+}
+
+inst fourByFourBoard {
+    Idx = I1 + I2 + I3 + I4
+    FirstIdx = I1
+    LastIdx = I4
+    next = I1->I2 + I2->I3 + I3->I4
 }
 
 inst sevenBySevenBoard {
@@ -85,5 +129,6 @@ inst sevenBySevenBoard {
     next = I1->I2 + I2->I3 + I3->I4 + I4->I5 + I5->I6 + I6->I7
 }
 
-// run { initSmallRoom } //for exactly 2 Player
-run { initFirstAndLast and traces } for sevenBySevenBoard
+run { initFirstAndLast and traces } for fourByFourBoard
+
+// TODO: predicate for wellFormed board ??

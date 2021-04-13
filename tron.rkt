51a965d490b0871697b4d514c04d1f7e57147075
bayazitdeniz <bayazitdeniz@gmail.com>
2021-04-13 10:37:43 -0400

    might have solved the player switching issue, but only tested on empty boards

--- tron.rkt
+++ tron.rkt
@@ -1,7 +1,10 @@
 #lang forge
 option problem_type temporal
-option max_tracelength 20
+option max_tracelength 30
 
+---------------------------------------------------------------------
+-- SIGS
+---------------------------------------------------------------------
 sig Idx {
     // right or below
     next: lone Idx
@@ -21,23 +24,11 @@ one sig Board {
     var currPlayer: one Player
 }
 
-// TODO: change fixed player start?
-pred initFirstAndLast {
-    no walls
-    --
-    P1.row = FirstIdx
-    P1.col = FirstIdx
-    --
-    P2.row = LastIdx
-    P2.col = LastIdx
-}
-
+---------------------------------------------------------------------
+-- Trace Transition Predicates
+---------------------------------------------------------------------
 fun otherPlayer[p : Player] : Player {
     Player - p
-}
-
-pred switchPlayers {
-    Board.(currPlayer') = otherPlayer[Board.currPlayer]
 }
 
 pred moveTo[p: Player, nextRow: Idx, nextCol: Idx] {
@@ -47,18 +38,17 @@ pred moveTo[p: Player, nextRow: Idx, nextCol: Idx] {
     some nextCol
     // no wall in the next location
     not (nextRow->nextCol in Board.walls)
-    // no player in the next location
-    let otherP = otherPlayer[p] | {
-        nextRow != otherP.row
-        nextCol != otherP.col
-    }
+    // impossible but - not the player's current location
+    (nextRow != p.row or nextCol != p.col)
+    // no other player in the next location
+    let otherP = otherPlayer[p] | (nextRow != otherP.row) or (nextCol != otherP.col)
 
     // post-conditions
     // curr loc will be in walls
     Board.(walls') = Board.walls + (p.row->p.col)
 
-    row' = (row - p->Idx) + p->nextRow
-    col' = (col - p->Idx) + p->nextCol
+    row' = (row - p->(p.row)) + p->nextRow
+    col' = (col - p->(p.col)) + p->nextCol
 }
 
 pred moveUp[p: Player] {
@@ -77,6 +67,25 @@ pred moveRight[p : Player] {
     moveTo[p, p.row, (p.col).next]
 }
 
+---------------------------------------------------------------------
+-- Trace Stutter and Validity (switching players) Predicates
+---------------------------------------------------------------------
+pred loserStutter[p : Player] {
+    let r = p.row | 
+        let c = p.col | 
+            let adjacents = (r.next)->c + (next.r)->c + r->(c.next) + r->(next.c) - none->Idx - Idx->none |
+                let oP = otherPlayer[p] |
+                    no (adjacents - Board.walls - (oP.row)->(oP.col))
+    walls' = walls
+    row' = row 
+    col' = col
+    currPlayer' = currPlayer
+}
+
+pred switchPlayers[curr : Board->Player] {
+    Board.curr' = otherPlayer[Board.curr]
+}
+
 // pred testTraces {
 //     moveUp[P2]
 //     after moveLeft[P2]
@@ -89,46 +98,167 @@ pred moveRight[p : Player] {
 //         until no P2.col.next
 // }
 
-pred loserStutter[p : Player] {
-    let r = p.row | 
-        let c = p.col | 
-            let adjacents = (r.next)->c + (next.r)->c + r->(c.next) + r->(next.c) - none->Idx - Idx->none |
-                let oP = otherPlayer[p]|
-                    no (adjacents - Board.walls - (oP.row)->(oP.col))
-    walls' = walls
-    row' = row 
-    col' = col
-    currPlayer' = currPlayer
-}
-
 pred traces {
     always (
-        moveUp[Board.currPlayer] or moveDown[Board.currPlayer] or moveLeft[Board.currPlayer] or moveRight[Board.currPlayer]
-        or loserStutter[Board.currPlayer]
+        ((moveUp[Board.currPlayer] or moveDown[Board.currPlayer] or moveLeft[Board.currPlayer] or moveRight[Board.currPlayer]) and switchPlayers[currPlayer]) or loserStutter[Board.currPlayer]
     )
 }
 
-inst threeByThreeBoard {
+---------------------------------------------------------------------
+-- Board Instances and Helpers
+---------------------------------------------------------------------
+
+inst twoByTwoEmptyBoard {
+    Idx = I1 + I2
+    FirstIdx = I1 
+    LastIdx = I2
+    next = I1->I2
+}
+
+inst threeByThreeEmptyBoard {
     Idx = I1 + I2 + I3
     FirstIdx = I1
     LastIdx = I3
     next = I1->I2 + I2->I3
 }
 
-inst fourByFourBoard {
+inst threeByThreeAlmostEndGame {
+    Board = Board0
+    Idx = I1 + I2 + I3
+    FirstIdx = I1
+    LastIdx = I3
+    next = I1->I2 + I2->I3
+    Player = P10 + P20
+    P1 = P10 
+    P2 = P20
+    --
+    walls = Board0->(I1->I1) + Board0->(I2->I1) + Board0->(I3->I1) + 
+            Board0->(I3->I3) + Board0->(I2->I3) + Board0->(I1->I3)
+    -- 
+    row = P10->I3 + P20->I1
+    col = P10->I2 + P20->I2
+}
+
+inst threeByThreeEndGame {
+    Board = Board0
+    Idx = I1 + I2 + I3
+    FirstIdx = I1
+    LastIdx = I3
+    next = I1->I2 + I2->I3
+    Player = P10 + P20
+    P1 = P10 
+    P2 = P20
+    --
+    walls = Board0->(I1->I1) + Board0->(I2->I1) + Board0->(I3->I1) + Board0->(I3->I2) +
+            Board0->(I3->I3) + Board0->(I2->I3) + Board0->(I1->I3) 
+    -- 
+    row = P10->I2 + P20->I1
+    col = P10->I2 + P20->I2
+}
+
+inst fourByFourEmptyBoard {
     Idx = I1 + I2 + I3 + I4
     FirstIdx = I1
     LastIdx = I4
     next = I1->I2 + I2->I3 + I3->I4
 }
 
-inst sevenBySevenBoard {
+// pred hardCodedSwitchForFour {
+//     Board.currPlayer = P1
+//     moveDown[P1]
+//     Board.(currPlayer') = P2
+//     after moveUp[P2]
+//     Board.(currPlayer'') = P1
+//     after after moveDown[P1]
+//     Board.(currPlayer''') = P2
+//     after after after moveUp[P2]
+//     Board.(currPlayer'''') = P1
+//     after after after after moveDown[P1]
+//     Board.(currPlayer''''') = P2
+//     after after after after after moveUp[P2]
+//     Board.(currPlayer'''''') = P1
+//     after after after after after after moveRight[P1]
+// }
+
+inst fourByFourPartFilledBoard {
+    Board = Board0
+    Idx = I1 + I2 + I3 + I4
+    FirstIdx = I1
+    LastIdx = I4
+    next = I1->I2 + I2->I3 + I3->I4
+    walls = Board0->(I1->I2) + Board0->(I3->I3)
+}
+
+inst fiveByFiveFullyFilledBoard {
+    Board = Board0
+    Idx = I1 + I2 + I3 + I4 + I5
+    FirstIdx = I1
+    LastIdx = I5
+    next = I1->I2 + I2->I3 + I3->I4 + I4->I5
+    walls = Board0->(Idx->Idx) - Board0->(FirstIdx->FirstIdx) - Board0->(LastIdx->LastIdx)
+}
+
+inst sevenBySevenEmptyBoard {
     Idx = I1 + I2 + I3 + I4 + I5 + I6 + I7
     FirstIdx = I1
     LastIdx = I7
     next = I1->I2 + I2->I3 + I3->I4 + I4->I5 + I5->I6 + I6->I7
 }
 
-run { initFirstAndLast and traces } for fourByFourBoard
+---------------------------------------------------------------------
+-- Initializations
+---------------------------------------------------------------------
 
-// TODO: predicate for wellFormed board ??
+pred initFirstAndLast {
+    no walls
+    --
+    P1.row = FirstIdx
+    P1.col = FirstIdx
+    --
+    P2.row = LastIdx
+    P2.col = LastIdx
+}
+
+pred p1First {
+    Board.currPlayer = P1
+}
+
+pred p2First {
+    Board.currPlayer = P2
+}
+
+// pred answer {
+//     Board.currPlayer = P1
+//     moveUp[P1]
+//     Board.currPlayer' = P2
+//     after (always loserStutter[P2])
+// }
+
+---------------------------------------------------------------------
+-- Running Experiments
+---------------------------------------------------------------------
+
+// Empty Board Runs
+run { p1First and initFirstAndLast and traces } for twoByTwoEmptyBoard
+run { p1First and initFirstAndLast and traces } for threeByThreeEmptyBoard
+run { p1First and initFirstAndLast and traces } for fourByFourEmptyBoard
+run { p1First and initFirstAndLast and traces } for sevenBySevenEmptyBoard
+
+// run { p2First and traces } for threeByThreeEndGame
+
+// run { answer and traces } for threeByThreeAlmostEndGame
+
+// run {p1FirstP2Second} for threeByThreeAlmostEndGame
+
+// run { hardCodedSwitch and initFirstAndLast and traces } for threeByThreeEmptyBoard
+
+// run { initFirstAndLast and traces } for fourByFourPartFilledBoard
+
+// run { initFirstAndLast and traces } for fiveByFiveFullyFilledBoard
+
+---------------------------------------------------------------------
+-- Future Work
+---------------------------------------------------------------------
+// TODO: predicate for wellFormed board ?
+// TODO: change fixed player start?
+// TODO: addition to viz to show who is currPlayer?

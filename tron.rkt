f7e691ed5c11850f33abf8c14ab8a6a92a91e8fb
bayazitdeniz <bayazitdeniz@gmail.com>
2021-04-19 16:27:22 -0400

    adding partial boards + validBoard predicate

--- tron.rkt
+++ tron.rkt
@@ -86,19 +86,19 @@ pred switchPlayers[curr : Board->Player] {
     Board.curr' = otherPlayer[Board.curr]
 }
 
-// pred testTraces {
-//     moveUp[P2]
-//     after moveLeft[P2]
-//     after after moveDown[P2]
-//     after after after moveLeft[P2]
-//     after after after after moveUp[P2]
-//     after after after after after moveUp[P2]
-//     after after after after after after {
-//         moveRight[P2]
-//         until no P2.col.next
-// }
+pred validBoard {
+    // next relation needs to be a list one beginning, one ending and everything connected
+    FirstIdx->(Idx - FirstIdx) in ^next
+    // players cannot be on a wall
+    all p: Player | {
+        not ((p.row)->(p.col) in Board.walls)
+    }
+    // players cannot be in the same location
+    not((P1.row = P2.row) and (P1.col = P2.col))
+}
 
 pred traces {
+    validBoard
     always (
         ((moveUp[Board.currPlayer] or moveDown[Board.currPlayer] or moveLeft[Board.currPlayer] or moveRight[Board.currPlayer]) and switchPlayers[currPlayer]) or loserStutter[Board.currPlayer]
     )
@@ -108,6 +108,7 @@ pred traces {
 -- Board Instances and Helpers
 ---------------------------------------------------------------------
 
+-- Empty Boards
 inst twoByTwoEmptyBoard {
     Idx = I1 + I2
     FirstIdx = I1 
@@ -136,81 +137,55 @@ inst sevenBySevenEmptyBoard {
     next = I1->I2 + I2->I3 + I3->I4 + I4->I5 + I5->I6 + I6->I7
 }
 
-// NOTE: cannot specify player location and walls in instance otherwise it doesn't var!
+-- Partial Boards
+inst threeByThreeAlmostEndGame {
+    Board = Board0
+    Idx = I1 + I2 + I3
+    FirstIdx = I1
+    LastIdx = I3
+    next = I1->I2 + I2->I3
+    --
+    walls ni Board0->(I1->I1) + Board0->(I2->I1) + Board0->(I3->I1) + Board0->(I3->I3) + Board0->(I2->I3) + Board0->(I1->I3)
+    walls in Board->Idx->Idx
+}
 
-// inst threeByThreeEndGame {
-//     Board = Board0
-//     Idx = I1 + I2 + I3
-//     FirstIdx = I1
-//     LastIdx = I3
-//     next = I1->I2 + I2->I3
-//     Player = P10 + P20
-//     P1 = P10 
-//     P2 = P20
-//     --
-//     Board0->(I1->I1) + Board0->(I2->I1) + Board0->(I3->I1) + Board0->(I3->I3) + Board0->(I2->I3) + Board0->(I1->I3) in walls
-//     -- 
-//     P10->I3 + P20->I1 in row
-//     P10->I2 + P20->I2 in col
-// }
+inst threeByThreeEndGame {
+    Board = Board0
+    Idx = I1 + I2 + I3
+    FirstIdx = I1
+    LastIdx = I3
+    next = I1->I2 + I2->I3
+    --
+    walls ni Board0->(I1->I1) + Board0->(I2->I1) + Board0->(I3->I1) + Board0->(I3->I2) + Board0->(I3->I3) + Board0->(I2->I3) + Board0->(I1->I3)
+    walls in Board->Idx->Idx
+}
 
-// inst threeByThreeAlmostEndGame {
-//     Board = Board0
-//     Idx = I1 + I2 + I3
-//     FirstIdx = I1
-//     LastIdx = I3
-//     next = I1->I2 + I2->I3
-//     Player = P10 + P20
-//     P1 = P10 
-//     P2 = P20
-//     --
-//     Board0->(I1->I1) + Board0->(I2->I1) + Board0->(I3->I1) + Board0->(I3->I2) + Board0->(I3->I3) + Board0->(I2->I3) + Board0->(I1->I3) in walls
-//     --
-//     P10->I2 + P20->I1 in row
-//     P10->I2 + P20->I2 in col
-// }
-
-// pred hardCodedSwitchForFour {
-//     Board.currPlayer = P1
-//     moveDown[P1]
-//     Board.(currPlayer') = P2
-//     after moveUp[P2]
-//     Board.(currPlayer'') = P1
-//     after after moveDown[P1]
-//     Board.(currPlayer''') = P2
-//     after after after moveUp[P2]
-//     Board.(currPlayer'''') = P1
-//     after after after after moveDown[P1]
-//     Board.(currPlayer''''') = P2
-//     after after after after after moveUp[P2]
-//     Board.(currPlayer'''''') = P1
-//     after after after after after after moveRight[P1]
-// }
-
-// inst fourByFourPartFilledBoard {
-//     Board = Board0
-//     Idx = I1 + I2 + I3 + I4
-//     FirstIdx = I1
-//     LastIdx = I4
-//     next = I1->I2 + I2->I3 + I3->I4
-//     walls = Board0->(I1->I2) + Board0->(I3->I3)
-// }
-
-// inst fiveByFiveFullyFilledBoard {
-//     Board = Board0
-//     Idx = I1 + I2 + I3 + I4 + I5
-//     FirstIdx = I1
-//     LastIdx = I5
-//     next = I1->I2 + I2->I3 + I3->I4 + I4->I5
-// }
+inst fourByFourPartFilledBoard {
+    Board = Board0
+    Idx = I1 + I2 + I3 + I4
+    FirstIdx = I1
+    LastIdx = I4
+    next = I1->I2 + I2->I3 + I3->I4
+    --
+    walls ni Board0->(I2->I2) + Board0->(I2->I3)
+    walls in Board->Idx->Idx
+}
 
 ---------------------------------------------------------------------
--- Initializations
+-- Initializations and Endings
 ---------------------------------------------------------------------
 
 pred initFirstAndLast {
     no walls
     --
+    P1.row = FirstIdx
+    P1.col = FirstIdx
+    --
+    P2.row = LastIdx
+    P2.col = LastIdx
+}
+
+pred initFirstAndLastSomeWalls {
     P1.row = FirstIdx
     P1.col = FirstIdx
     --
@@ -226,33 +201,30 @@ pred p2First {
     Board.currPlayer = P2
 }
 
+pred p2LosesLater {
+    not (after after after loserStutter[P2])
+    eventually loserStutter[P2]
+}
+
 ---------------------------------------------------------------------
 -- Running Experiments
 ---------------------------------------------------------------------
 
 -- Empty Board Runs
 // run { p1First and initFirstAndLast and traces } for twoByTwoEmptyBoard
-//run { p1First and initFirstAndLast and traces } for threeByThreeEmptyBoard
+// run { p1First and initFirstAndLast and traces } for threeByThreeEmptyBoard
 // run { p1First and initFirstAndLast and traces } for fourByFourEmptyBoard
 // run { p1First and initFirstAndLast and traces } for sevenBySevenEmptyBoard
+
+-- EndGame or Partial Boards runs to debug transitions
+// run { p2First and traces } for threeByThreeEndGame
+// run { p1First and traces } for threeByThreeAlmostEndGame
+run { p1First and initFirstAndLastSomeWalls and traces and p2LosesLater } for fourByFourPartFilledBoard
 
 ---------------------------------------------------------------------
 -- Basic Tests
 ---------------------------------------------------------------------
-
-
-// TODO: turn all of the partly filled with walls instances into tests,
-//       the problem with partial maps is that if you specify walls 
-//       it will consider them as static throughout the trace!
-//       Why can't we put it in preds? Because we need the atoms specifying the Idx
-
--- EndGame runs to debug transitions
-// run { p2First and traces } for threeByThreeEndGame
-// run { p1First and traces } for threeByThreeAlmostEndGame
-// run { p1First and traces } for threeByThreeAlmostEndGame2
-// run { hardCodedSwitch and initFirstAndLast and traces } for threeByThreeEmptyBoard
-// run { initFirstAndLast and traces } for fourByFourPartFilledBoard
-// run { initFirstAndLast and traces } for fiveByFiveFullyFilledBoard
+option verbose 0
 
 test expect {
     moveDownTest: {
@@ -384,8 +356,24 @@ test expect {
         
     } is sat
 
-
 }
+
+// pred hardCodedSwitchForFour {
+//     Board.currPlayer = P1
+//     moveDown[P1]
+//     Board.(currPlayer') = P2
+//     after moveUp[P2]
+//     Board.(currPlayer'') = P1
+//     after after moveDown[P1]
+//     Board.(currPlayer''') = P2
+//     after after after moveUp[P2]
+//     Board.(currPlayer'''') = P1
+//     after after after after moveDown[P1]
+//     Board.(currPlayer''''') = P2
+//     after after after after after moveUp[P2]
+//     Board.(currPlayer'''''') = P1
+//     after after after after after after moveRight[P1]
+// }
 
 ---------------------------------------------------------------------
 -- Verifications
@@ -399,10 +387,3 @@ test expect {
     alwaysAWinner2: { p2First and initFirstAndLast and traces and eventually loserStutter[Board.currPlayer]} is sat
 
 }
-
----------------------------------------------------------------------
--- Future Work
----------------------------------------------------------------------
-// TODO: predicate for wellFormed board ?
-// TODO: change fixed player start?
-// TODO: addition to viz to show who is currPlayer?
